0cdd388ce68587c536b6331b07b795c8bf780f37
lucasadesky <64547674+lucasadesky@users.noreply.github.com>
2023-06-02 17:27:09 -0400

    started graph implementation

--- main2.py
+++ main2.py
@@ -11,15 +11,28 @@ pygame.display.init()
 
 screen = pygame.display.set_mode((width, height))
 
+# graph
+components = {}
+
 # not currently used
 wire_tracking = []
 
 # list to contain all components currently on the screen
-components = []
+list_components = []
 
-components.append(Battery(screen, wire_tracking, (200, 200)))
-# components.append(Battery(screen, wire_tracking, (300, 300), color = "red"))
-components.append(LED(screen, wire_tracking, (400, 200)))
+first_component = Battery(screen, wire_tracking, (200, 200))
+
+def addLED (to_component): 
+    components[to_component] = components.get(to_component, []) + [LED(screen, wire_tracking, (random.randint(50, width-50), random.randint(50, height-50)))]
+
+def addBattery (to_component): 
+    components[to_component] = components.get(to_component, []) + [Battery(screen, wire_tracking, (random.randint(50, width-50), random.randint(50, height-50)))]
+
+addLED(first_component)
+
+# list_components.append(Battery(screen, wire_tracking, (200, 200)))
+# # components.append(Battery(screen, wire_tracking, (300, 300), color = "red"))
+# list_components.append(LED(screen, wire_tracking, (400, 200)))
 
 
 elementDrag = False
@@ -34,8 +47,18 @@ while True:
     # fill background
     screen.fill("lightcyan1")
 
+    # for component in list_components:
+    #     component.draw()
+
+
+
+    for component in components:
+        for connection in components[component]:
+            pygame.draw.line(screen, "black", component.position, connection.position, 4)
+    
     for component in components:
         component.draw()
+
 
 
     for event in pygame.event.get():
@@ -48,9 +71,11 @@ while True:
                 pygame.display.quit()
                 pygame.quit()
             elif event.key == pygame.K_b:
-                components.append(Battery(screen, wire_tracking, (random.randint(50, width-50), random.randint(50, height-50))))
+                addBattery(first_component)
+                print("need to change it from adding to first component to adding to the selected component")
             elif event.key == pygame.K_l:
-                components.append(LED(screen, wire_tracking, (random.randint(50, width-50), random.randint(50, height-50))))
+                addLED(first_component)
+                print("need to change it from adding to first component to adding to the selected component")
             elif event.key == pygame.K_SPACE:
                 breakMode = True
                 print("Click a component to break its connection(s)")
@@ -84,33 +109,34 @@ while True:
                         breakMode = False
                         break
 
-            for component in components:
-                if component.checkClick(pygame.mouse.get_pos()):
-                    indextodrag = components.index(component)
-                    elementDrag = True
-                    break
-                elif component.checkNodeClick(pygame.mouse.get_pos()):
-                    indextodrag = components.index(component)
-                    nodeDrag = True
-                    break
+
+            # for component in components:
+            #     if component.checkClick(pygame.mouse.get_pos()):
+            #         indextodrag = list_components.index(component)
+            #         elementDrag = True
+            #         break
+            #     elif component.checkNodeClick(pygame.mouse.get_pos()):
+            #         indextodrag = list_components.index(component)
+            #         nodeDrag = True
+            #         break
             
 
 
-        elif event.type == pygame.MOUSEMOTION:
-            if nodeDrag:
-                components[indextodrag].dragNode(pygame.mouse.get_pos())
-                # if a cathode is dragged over the anode of another component, connect them
-                for component in components:
-                    if component.checkNodeClick(pygame.mouse.get_pos()) and (component != components[indextodrag]):
-                        if components[indextodrag].wire_out.cathode.checkClick(pygame.mouse.get_pos()):
-                            components[indextodrag].connect("cathode", component)
-                        elif components[indextodrag].wire_in.anode.checkClick(pygame.mouse.get_pos()):
-                            components[indextodrag].connect("anode", component)
-                        break
+        # elif event.type == pygame.MOUSEMOTION:
+        #     if nodeDrag:
+        #         list_components[indextodrag].dragNode(pygame.mouse.get_pos())
+        #         # if a cathode is dragged over the anode of another component, connect them
+        #         for component in list_components:
+        #             if component.checkNodeClick(pygame.mouse.get_pos()) and (component != list_components[indextodrag]):
+        #                 if list_components[indextodrag].wire_out.cathode.checkClick(pygame.mouse.get_pos()):
+        #                     list_components[indextodrag].connect("cathode", component)
+        #                 elif list_components[indextodrag].wire_in.anode.checkClick(pygame.mouse.get_pos()):
+        #                     list_components[indextodrag].connect("anode", component)
+        #                 break
                 
 
-            elif elementDrag:
-                components[indextodrag].dragComponent(pygame.mouse.get_pos())
+        #     elif elementDrag:
+        #         list_components[indextodrag].dragComponent(pygame.mouse.get_pos())
 
         elif event.type == pygame.MOUSEBUTTONUP:
             elementDrag = False
